adec6aaaf479c168f96abf3ef841e6ef240043a2
User <senamela@wamly.io>
2022-07-17 16:04:38 +0200

    settings, migrations

--- database/migrations/2022_07_17_132437_create_settings_table.php
+++ database/migrations/2022_07_17_132437_create_settings_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('description');
             $table->timestamps();
         });
     }
