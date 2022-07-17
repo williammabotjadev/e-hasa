60e1c1537f228a39f55950ece57ebaebf21a6395
User <senamela@wamly.io>
2022-07-17 15:15:06 +0200

    classroom, model"

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Classroom;
 
 class Schedule extends Model
 {
     use HasFactory;
+
+    public function classrooms()
+    {
+        return $this->hasMany(Classroom::class);
+    }
 
     protected $fillable = [
         'name',
