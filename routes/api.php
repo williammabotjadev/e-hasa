35c2d6bb799b387d1d119fdcbc72c5192e8d73f8
User <senamela@wamly.io>
2022-07-17 18:31:09 +0200

    resources, crud, all moels

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('classrooms', App\Http\Controllers\ClassroomsController::class);
+Route::resource('students', App\Http\Controllers\StudentsController::class);
+Route::resource('curriculums', App\Http\Controllers\CurriculumsController::class);
+Route::resource('schedules', App\Http\Controllers\SchedulesController::class);
+Route::resource('resources', App\Http\Controllers\ResourcesController::class);
+Route::resource('messages', App\Http\Controllers\MessagesController::class);
+Route::resource('settings', App\Http\Controllers\SettingsController::class);
