df69ef98e78459d059cabda66f122841184504ba
User <senamela@wamly.io>
2022-07-17 18:48:09 +0200

    import user repo

--- app/Http/Controllers/InstructorsController.php
+++ app/Http/Controllers/InstructorsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\UserRepository;
 
 class InstructorsController extends Controller
 {
--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\UserRepository;
 
 class StudentsController extends Controller
 {
