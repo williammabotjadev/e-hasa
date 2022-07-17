28c983d1c7d86f1e636953e1ac5dd3323ff09344
User <senamela@wamly.io>
2022-07-17 15:16:06 +0200

    user, setting

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Setting extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
     protected $fillable = [
         'name',
