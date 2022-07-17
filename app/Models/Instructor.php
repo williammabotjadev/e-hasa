56839d47ccfadc6a50f45cf1b71e30406f68dda3
User <senamela@wamly.io>
2022-07-17 15:13:18 +0200

    user, instructor model

--- app/Models/Instructor.php
+++ app/Models/Instructor.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Instructor extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'phone',
+        'address'
+    ];
 }
