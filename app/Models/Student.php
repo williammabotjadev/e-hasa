4d51120e66b5e24ba8961f6cbbeb6e6a6d38b8b8
User <senamela@wamly.io>
2022-07-17 15:17:05 +0200

    user, student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Student extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
 
     protected $fillable = [
         'firstname',
